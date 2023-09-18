2ee57efd4a036515ca08f3fd92547273a5eb8c28
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-09-18 17:01:57 +0600

    header

---
+++ script.js
@@ -0,0 +1,17 @@
+function loadPage(page) {
+    // You can implement logic here to load content based on the page
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = `<h2>${page} Page Content</h2>`;
+}
+
+function handleNavigation() {
+    const hash = window.location.hash;
+    const page = hash.substring(1); // Remove the "#" symbol
+    loadPage(page);
+}
+
+// Initial page load
+handleNavigation();
+
+// Listen for changes in the URL (hash changes)
+window.addEventListener('hashchange', handleNavigation);
