319aa961bc33745998a2d9cc68e254e8cdc610da
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-09-19 01:35:21 +0600

    10 section complete

--- result.php
+++ result.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template</title>
+    
     <script src="https://kit.fontawesome.com/64340c05bf.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/owl.carousel.min.css">
 
     <link rel="stylesheet" href="style.css">
+    <title>অলংকার পুর আদর্শ মাধ্যমিক বিদ্যালয়<</title>
 </head>
 
 <body>
@@ -42,43 +43,88 @@
 
     <!-- navbar section start -->
     <section class="container">
-        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-sm navbar-dark text-primary bg-secondary navbar-sticky-top">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="mynavbar">
+                <div class="collapse navbar-collapse text-primary" id="mynavbar">
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" href="index.html">HOME</a>
+
+                            <button class="  btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary active" href="#">HOME</a>
+
+                            </button>
+
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.html">ABOUT US</a>
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary" href="http://127.0.0.1:5500/about.html">ABOUT US</a>
+
+                            </button>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">STUDENT CORNER</a>
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary dropdown-toggle" data-bs-toggle="dropdown"
+                                    href="#">STUDENT CORNER</a>
+
+                            </button>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Our Student</a>  </li>
-                                <li><a class="dropdown-item" href="#">Feesh pyment</a> </li>
-                                
-                                <li><a class="dropdown-item" href="#">Dress info</a></li>
-                                <li><a class="dropdown-item" href="#">Class Rutting</a></li>
-                                <li><a class="dropdown-item" href="#">Exam Rutting</a></li>
-                               
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/student.html">Our Student</a> </li>
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/Notic/notic1.html">Feesh
+                                        pyment</a> </li>
+
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/student/dress.html">Dress
+                                        info</a></li>
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/student/rutting.html">Class
+                                        Rutting</a></li>
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/student/rutting.html">Exam Rutting</a></li>
+                                <li><a class="dropdown-item" href="http://127.0.0.1:5500/attentends.html">Attentends </a></li>
+
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">RESULTS</a>
+                            <button class="btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary"
+                                    href="http://localhost/Alonkerpur_Ideal_High_School/codeimg/index.php">RESULTS</a>
+
+                            </button>
+
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="galary.html">GALLERY</a>
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary" href="http://127.0.0.1:5500/galary.html">GALLERY</a>
+
+
+                            </button>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">CONTACT</a>
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary" href="http://127.0.0.1:5500/contact.html">CONTACT</a>
+
+                            </button>
                         </li>
-                       
+                        <li class="nav-item">
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary"
+                                    href="http://127.0.0.1:5500/Notic/notic1.html">PYMENT</a>
+
+                            </button>
+                        </li>
+                        <li class="nav-item">
+                            <button class=" btn-outline-dark">
+                                <a class="nav-link  fw-bold text-primary" href="http://127.0.0.1:5500/techers.html">TECHERS</a>
+
+                            </button>
+                        </li>
+
+
+
+
                     </ul>
                     <form class="action_page.php d-flex">
+
                         <a href="#mymodel" class="btn btn-primary me-2" data-bs-toggle="modal">Log-In</a>
                         <a href="#mymode2" data-bs-toggle="modal" class="btn btn-primary">Sign-Up</a>
                     </form>
